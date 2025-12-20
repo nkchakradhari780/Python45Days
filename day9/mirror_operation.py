1cfd804d24dd61b4426d1007f907661b7e120422
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2025-12-20 19:04:29 +0530

    dictionary question

--- day9/mirror_operation.py
+++ day9/mirror_operation.py
@@ -1,1 +1,19 @@
- 
+input_string = input("Enter strinng: ")
+n = int(input("Enter N: "))
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+reverse_alphabets = alphabets[::-1]
+
+dict1 = dict(zip(alphabets, reverse_alphabets))
+
+
+prefix = input_string[0: n-1]
+suffix = input_string[n-1:]
+
+
+mirror = ""
+for i in range(0, len(suffix)):
+    mirror = mirror + dict1[suffix[i]]
+
+res = prefix + mirror
+print("The result is: ", res)
