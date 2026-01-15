e734392ac2a080d2d25e7fe843b22caddfd495f5
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2026-01-15 11:00:26 +0530

    formatting and concatenating the string

---
+++ day12/modifying_strings2.py
@@ -0,0 +1,19 @@
+str1 = "Hello Nkchakradhari "
+str2 = "what a great place this is"
+
+str3 = str1 + str2
+
+print(str3)
+
+# string formattig 
+# used to insert variable values in a string
+
+Name = "Nitin"
+Marks = 98
+
+str1 = "The student name is {s}, and marks is {m}".format(s = Name, m = Marks)
+print(str1)
+
+# Escape characters 
+# special characters thatn are used to represent some non printable or reserved characters in a string
+
