14ca0cbff865ad4db365c27e2d0c383901069ed6
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2026-01-15 10:43:56 +0530

    modifying the string

---
+++ day12/modifying_strings.py
@@ -0,0 +1,18 @@
+# for converting characters to upper case 
+str1 = "new yourk"
+
+str2 = str1.upper()
+print(str2)
+
+# for converting characters to lower case 
+str3 = str2.lower()
+print(str3)
+
+# for capatalizing the first character of the string 
+str4 = str3.capitalize()
+print(str4)
+
+# for stripping/removing any trailing white spaces
+str1 = "       Hello Nkchakradhari     "
+str2 = str1.strip()
+print(str2)
