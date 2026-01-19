697a7c412b832aa5dbb6261c3d7af6805a1e3d6a
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2026-01-19 23:19:34 +0530

    opening file

--- day16/openFile.py
+++ day16/openFile.py
@@ -9,3 +9,19 @@ if f:
     print("file successffully open")
 
 print(type(f))
+
+# buffering ->  Positive interger value used to set bugger size for file
+# it should be 1 or more than 1 in text mode
+# in binary mode buffer size can be 0
+# defaudefault buggering size := 4096-8192
+
+# encoding -> encoding type used to decode and encode file
+# should be used in text mode only
+# default depends on OS 
+# for windows cp1252
+ 
+# errors -> represents how encodeing and decoding errors are to be handled
+# cannot be used in binary mode 
+# some standard values are -> strict, ignore, replace etc.
+
+# newLine -> \n, \r, \r\n
---
+++ day16/openFile2.py
@@ -0,0 +1,4 @@
+f = open('hello.txt', mode='r',buffering=10,encoding='utf-8')
+if f:
+    print("openned")
+print(f)
